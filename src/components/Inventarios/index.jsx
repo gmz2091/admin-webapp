f11b3c93ab0cbfbacf67ea6e63dd6228a4dc6607
Magdiel <mgomez2091@gmail.com>
2021-11-15 13:40:53 -0700

    Added Firebase Storage

--- src/components/Inventarios/index.jsx
+++ src/components/Inventarios/index.jsx
@@ -49,32 +49,84 @@ const Inventario = () => {
         </div>
         <div className="w-10/12 m-auto">
           {inventario.length > 0 ? (
-            inventario.map((product) => (
-              <div className="w-full py-12" key={product.id}>
-                <div className="w-full flex flex-row justify-between">
-                  <div className="w-1/2">
-                    <p className="text-lg">Producto</p>
-                    <h1 className="text-center">{product.name}</h1>
-                  </div>
-                  <div className="w-1/2">
-                    <p className="text-lg">Precio</p>
-                    <h1 className="text-center">{product.price}</h1>
-                  </div>
-                </div>
-                <div className="w-full flex flex-row justify-between">
-                  <div className="w-1/2">
-                    <p className="text-lg">Stock</p>
-                    <h1 className="text-center">{product.quantity}</h1>
-                  </div>
-                  <div className="w-1/2">
-                    <p className="text-lg">Categoria</p>
-                    <h1 className="text-center">{product.category}</h1>
+            <div className="flex flex-col">
+              <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                  <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                    <table className="min-w-full divide-y divide-gray-200">
+                      <thead className="bg-gray-50">
+                        <tr>
+                          <th
+                            scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                          >
+                            Name
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                          >
+                            Title
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                          >
+                            Status
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                          >
+                            Role
+                          </th>
+                          <th scope="col" className="relative px-6 py-3">
+                            <span className="sr-only">Edit</span>
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody className="bg-white divide-y divide-gray-200">
+                        {inventario.map((product) => (
+                          <tr key={product.id}>
+                            <td className="px-6 py-4 whitespace-nowrap">
+                              <div className="flex items-center">
+                                <div
+                                  className="flex-shrink-0 h-10 w-10"
+                                >
+                                  <img className="h-10 w-10" src={product.image} alt="" />
+                                </div>
+                                <div className="ml-4">
+                                  <div className="text-sm font-medium text-gray-900">{product.name}</div>
+                                  <div className="text-sm text-gray-500">{product.description}</div>
+                                </div>
+                              </div>
+                            </td>
+                            <td className="px-6 py-4 whitespace-nowrap">
+                              <div className="text-sm text-gray-900">{product.price}</div>
+                              <div className="text-sm text-gray-500">{product.category}</div>
+                            </td>
+                            <td className="px-6 py-4 whitespace-nowrap">
+                              <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                Active
+                              </span>
+                            </td>
+                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{product.description}</td>
+                            <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                              <p
+                                className="text-indigo-600 hover:text-indigo-900"
+                              >
+                                Edit
+                              </p>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
                   </div>
                 </div>
               </div>
-            ))
+            </div>
           ) : ('No hay Products')}
-
         </div>
       </div>
     </>
